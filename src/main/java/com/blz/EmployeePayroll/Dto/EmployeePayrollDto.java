6a022a27e940d2d30ebdae93ada5b1f02e04f1e6
Arun <arungawande1995@gmail.com>
2022-08-19 22:17:01 +0530

    Added: lombok and getter setters DB setting Enviroment variable

---
+++ src/main/java/com/blz/EmployeePayroll/Dto/EmployeePayrollDto.java
@@ -0,0 +1,17 @@
+package com.blz.EmployeePayroll.Dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeePayrollDto {
+    //	private long id;
+    private String name;
+    private double salary;
+
+    private String emailId;
+    private String pwd;
+
+//    private DepartmentDto department;
+
+
+}
